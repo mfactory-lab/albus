299c1e2dc29bb4bf550110c0194b7448e53d4bf8
tiamo <vk.tiamo@gmail.com>
2023-05-04 23:30:41 +0300

    fix(program): add new authority

--- programs/albus/src/constants.rs
+++ programs/albus/src/constants.rs
@@ -1,5 +1,6 @@
 pub const AUTHORIZED_AUTHORITY: &[&str] = &[
-    "4kMtMnYWFbsMc7M3jcdnfCceHaiXmrqaMz2QZQAmn88i", // ...
+    "4kMtMnYWFbsMc7M3jcdnfCceHaiXmrqaMz2QZQAmn88i",
+    "5tWk9EZcMpdKzxVGr4ZakZDHdWiqVJkLQ1b3v2vraDeH",
 ];
 
 pub const NFT_SYMBOL_PREFIX: &str = "ALBUS";
