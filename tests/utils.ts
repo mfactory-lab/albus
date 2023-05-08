c1019debd05b8a3f0cdacd033367728987ba939d
tiamo <vk.tiamo@gmail.com>
2023-05-08 18:04:32 +0300

    chore: cleanup

--- tests/utils.ts
+++ tests/utils.ts
@@ -6,13 +6,12 @@ import { assert } from 'chai'
 
 export const payerKeypair = web3.Keypair.fromSecretKey(Uint8Array.from([46, 183, 156, 94, 55, 128, 248, 0, 49, 70, 183, 244, 178, 0, 0, 236, 212, 131, 76, 78, 112, 48, 25, 79, 249, 33, 43, 158, 199, 2, 168, 18, 55, 174, 166, 159, 57, 67, 197, 158, 255, 142, 177, 177, 47, 39, 35, 185, 148, 253, 191, 58, 219, 119, 104, 89, 225, 26, 244, 119, 160, 6, 156, 227]))
 
-const opts = AnchorProvider.defaultOptions()
-
 export function newProvider(payerKeypair: web3.Keypair) {
+  const opts = AnchorProvider.defaultOptions()
   return new AnchorProvider(
-    new web3.Connection('http://localhost:8899', opts.preflightCommitment),
+    new web3.Connection('http://localhost:8899', opts),
     new Wallet(payerKeypair),
-    AnchorProvider.defaultOptions(),
+    opts,
   )
 }
 
