9bacd7d9afac91c7afcf680a63244f73c6058af1
tiamo <vk.tiamo@gmail.com>
2023-05-11 19:33:32 +0300

    chore: add copyright

--- tests/verifiedTransfer.ts
+++ tests/verifiedTransfer.ts
@@ -1,3 +1,31 @@
+/*
+ * This file is part of Albus code.
+ *
+ * Copyright (c) 2023, jFactory GmbH
+ *
+ * Albus is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Albus is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ *
+ * You can be released from the requirements of the Affero GNU General Public License
+ * by purchasing a commercial license. The purchase of such a license is
+ * mandatory as soon as you develop commercial activities using the
+ * Albus code without disclosing the source code of
+ * your own applications.
+ *
+ * The developer of this program can be contacted at <info@jfactory.ch>.
+ */
+
 import { Metaplex, keypairIdentity } from '@metaplex-foundation/js'
 import { BN } from '@project-serum/anchor'
 import { TOKEN_PROGRAM_ID, createMint, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token'
