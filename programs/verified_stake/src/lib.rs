47f88b7c04a9b9912d39bf83f4b6f8998c96b98c
kazinarka <illia.fedotov@nure.ua>
2023-05-23 12:27:01 +0300

    feat(verified-stake program): impl additional instructions

--- programs/verified_stake/src/lib.rs
+++ programs/verified_stake/src/lib.rs
@@ -33,53 +33,24 @@ declare_id!("CMev81L3acPrcTTevCFGdcNQnDypMGzuiAUgo8NBZJzr");
 #[program]
 pub mod verified_stake {
     use albus_verifier::check_compliant;
+    use anchor_lang::prelude::borsh::{BorshDeserialize, BorshSerialize};
     use anchor_lang::solana_program::program::invoke;
 
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<VerifiedInitialize>,
-        authorized: stake::state::Authorized,
-        lockup: stake::state::Lockup,
-    ) -> Result<()> {
-        check_compliant(
-            &ctx.accounts.zkp_request,
-            Some(ctx.accounts.authority.key()),
-        )?;
-
-        let ix = stake::instruction::initialize(&ctx.accounts.stake.key(), &authorized, &lockup);
-
-        let account_infos = vec![
-            ctx.accounts.stake.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
-        ];
-
-        invoke(&ix, &account_infos)?;
-
-        Ok(())
+    #[derive(BorshSerialize, BorshDeserialize)]
+    pub enum StakeAuthorize {
+        Staker,
+        Withdrawer,
     }
 
-    pub fn initialize_checked(
-        ctx: Context<VerifiedInitializeChecked>,
-        authorized: stake::state::Authorized,
-    ) -> Result<()> {
-        check_compliant(
-            &ctx.accounts.zkp_request,
-            Some(ctx.accounts.withdrawer.key()),
-        )?;
-
-        let ix = stake::instruction::initialize_checked(&ctx.accounts.stake.key(), &authorized);
-
-        let account_infos = vec![
-            ctx.accounts.stake.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
-            ctx.accounts.staker.to_account_info(),
-            ctx.accounts.withdrawer.to_account_info(),
-        ];
-
-        invoke(&ix, &account_infos)?;
-
-        Ok(())
+    impl Into<stake::state::StakeAuthorize> for StakeAuthorize {
+        fn into(self) -> stake::state::StakeAuthorize {
+            match self {
+                StakeAuthorize::Staker => stake::state::StakeAuthorize::Staker,
+                StakeAuthorize::Withdrawer => stake::state::StakeAuthorize::Withdrawer
+            }
+        }
     }
 
     pub fn split(ctx: Context<VerifiedSplit>, lamports: u64) -> Result<()> {
@@ -95,6 +66,7 @@ pub mod verified_stake {
             &ctx.accounts.split_stake.key(),
         );
 
+        // determine that the number of instructions is equal to 3
         assert_eq!(ixs.len(), 3);
 
         let allocate_account_infos = vec![ctx.accounts.split_stake.to_account_info()];
@@ -116,6 +88,43 @@ pub mod verified_stake {
         Ok(())
     }
 
+    pub fn split_with_seed(ctx: Context<VerifiedSplitWithSeed>, lamports: u64, seed: String) -> Result<()> {
+        check_compliant(
+            &ctx.accounts.zkp_request,
+            Some(ctx.accounts.authorized.key()),
+        )?;
+
+        let ixs = stake::instruction::split_with_seed(
+            &ctx.accounts.stake.key(),
+            &ctx.accounts.authorized.key(),
+            lamports,
+            &ctx.accounts.split_stake.key(),
+            &ctx.accounts.base.key(),
+            &seed,
+        );
+
+        // determine that the number of instructions is equal to 2
+        assert_eq!(ixs.len(), 2);
+
+        let allocate_account_infos = vec![
+            ctx.accounts.split_stake.to_account_info(),
+            ctx.accounts.base.to_account_info(),
+        ];
+
+        let split_account_infos = vec![
+            ctx.accounts.stake.to_account_info(),
+            ctx.accounts.split_stake.to_account_info(),
+            ctx.accounts.authorized.to_account_info(),
+        ];
+
+        let (allocate_ix, split_ix) = (&ixs[0], &ixs[1]);
+
+        invoke(&allocate_ix, &allocate_account_infos)?;
+        invoke(&split_ix, &split_account_infos)?;
+
+        Ok(())
+    }
+
     pub fn merge(ctx: Context<VerifiedMerge>) -> Result<()> {
         check_compliant(
             &ctx.accounts.zkp_request,
@@ -128,6 +137,7 @@ pub mod verified_stake {
             &ctx.accounts.authorized.key(),
         );
 
+        // determine that the number of instructions is equal to 1
         assert_eq!(ixs.len(), 1);
 
         let account_infos = vec![
@@ -180,28 +190,6 @@ pub mod verified_stake {
         Ok(())
     }
 
-    pub fn deactivate(ctx: Context<VerifiedDeactivate>) -> Result<()> {
-        check_compliant(
-            &ctx.accounts.zkp_request,
-            Some(ctx.accounts.authorized.key()),
-        )?;
-
-        let ix = stake::instruction::deactivate_stake(
-            &ctx.accounts.stake.key(),
-            &ctx.accounts.authorized.key(),
-        );
-
-        let account_infos = vec![
-            ctx.accounts.stake.to_account_info(),
-            ctx.accounts.clock.to_account_info(),
-            ctx.accounts.authorized.to_account_info(),
-        ];
-
-        invoke(&ix, &account_infos)?;
-
-        Ok(())
-    }
-
     pub fn delegate(ctx: Context<VerifiedDelegate>) -> Result<()> {
         check_compliant(
             &ctx.accounts.zkp_request,
@@ -241,6 +229,7 @@ pub mod verified_stake {
             &ctx.accounts.uninitialized_stake.key(),
         );
 
+        // determine that the number of instructions is equal to 3
         assert_eq!(ixs.len(), 3);
 
         let allocate_account_infos = vec![ctx.accounts.uninitialized_stake.to_account_info()];
@@ -264,55 +253,143 @@ pub mod verified_stake {
         Ok(())
     }
 
-    #[derive(Accounts)]
-    pub struct VerifiedInitialize<'info> {
-        /// CHECK:
-        pub authority: Signer<'info>,
+    pub fn redelegate_with_seed(ctx: Context<VerifiedRedelegateWithSeed>, seed: String) -> Result<()> {
+        check_compliant(
+            &ctx.accounts.zkp_request,
+            Some(ctx.accounts.authorized.key()),
+        )?;
 
-        /// CHECK:
-        #[account(mut)]
-        pub stake: AccountInfo<'info>,
+        let ixs = stake::instruction::redelegate_with_seed(
+            &ctx.accounts.stake.key(),
+            &ctx.accounts.authorized.key(),
+            &ctx.accounts.vote.key(),
+            &ctx.accounts.uninitialized_stake.key(),
+            &ctx.accounts.base.key(),
+            &seed,
+        );
 
-        /// CHECK:
-        pub rent: AccountInfo<'info>,
+        // determine that the number of instructions is equal to 2
+        assert_eq!(ixs.len(), 2);
 
-        /// CHECK: Albus ZKP request
-        pub zkp_request: AccountInfo<'info>,
+        let allocate_account_infos = vec![
+            ctx.accounts.uninitialized_stake.to_account_info(),
+            ctx.accounts.base.to_account_info(),
+        ];
 
-        /// CHECK:
-        pub stake_program: AccountInfo<'info>,
+        let redelegate_account_infos = vec![
+            ctx.accounts.stake.to_account_info(),
+            ctx.accounts.uninitialized_stake.to_account_info(),
+            ctx.accounts.vote.to_account_info(),
+            ctx.accounts.stake_config.to_account_info(),
+            ctx.accounts.authorized.to_account_info(),
+        ];
+
+        let (allocate_ix, redelegate_ix) = (&ixs[0], &ixs[1]);
+
+        invoke(&allocate_ix, &allocate_account_infos)?;
+        invoke(&redelegate_ix, &redelegate_account_infos)?;
+
+        Ok(())
     }
 
-    #[derive(Accounts)]
-    pub struct VerifiedInitializeChecked<'info> {
-        /// CHECK:
-        pub staker: AccountInfo<'info>,
+    pub fn authorize<'info>(ctx: Context<'_, '_, '_, 'info, VerifiedAuthorize<'info>>, new_authorized: Pubkey, stake_authorize: StakeAuthorize) -> Result<()> {
+        check_compliant(
+            &ctx.accounts.zkp_request,
+            Some(ctx.accounts.authorized.key()),
+        )?;
 
-        /// CHECK:
-        pub withdrawer: Signer<'info>,
+        let mut account_infos = vec![
+            ctx.accounts.stake.to_account_info(),
+            ctx.accounts.clock.to_account_info(),
+            ctx.accounts.authorized.to_account_info(),
+        ];
 
-        /// CHECK:
-        #[account(mut)]
-        pub stake: AccountInfo<'info>,
+        let mut custodian_pubkey = None;
 
-        /// CHECK:
-        pub rent: AccountInfo<'info>,
+        if let Some(custodian) = ctx.remaining_accounts.get(0) {
+            custodian_pubkey = Some(custodian.key());
+            account_infos.push(custodian.to_account_info());
+        };
 
-        /// CHECK: Albus ZKP request
-        pub zkp_request: AccountInfo<'info>,
+        let ix = stake::instruction::authorize(
+            &ctx.accounts.stake.key(),
+            &ctx.accounts.authorized.key(),
+            &new_authorized,
+            stake_authorize.into(),
+            custodian_pubkey.as_ref(),
+        );
 
-        /// CHECK:
-        pub stake_program: AccountInfo<'info>,
+        invoke(&ix, &account_infos)?;
+
+        Ok(())
+    }
+
+    pub fn authorize_checked<'info>(ctx: Context<'_, '_, '_, 'info, VerifiedAuthorizeChecked<'info>>, stake_authorize: StakeAuthorize) -> Result<()> {
+        check_compliant(
+            &ctx.accounts.zkp_request,
+            Some(ctx.accounts.authorized.key()),
+        )?;
+
+        let mut account_infos = vec![
+            ctx.accounts.stake.to_account_info(),
+            ctx.accounts.clock.to_account_info(),
+            ctx.accounts.authorized.to_account_info(),
+            ctx.accounts.new_authorized.to_account_info(),
+        ];
+
+        let mut custodian_pubkey = None;
+
+        if let Some(custodian) = ctx.remaining_accounts.get(0) {
+            custodian_pubkey = Some(custodian.key());
+            account_infos.push(custodian.to_account_info());
+        };
+
+        let ix = stake::instruction::authorize_checked(
+            &ctx.accounts.stake.key(),
+            &ctx.accounts.authorized.key(),
+            &ctx.accounts.new_authorized.key(),
+            stake_authorize.into(),
+            custodian_pubkey.as_ref(),
+        );
+
+        invoke(&ix, &account_infos)?;
+
+        Ok(())
     }
 
     #[derive(Accounts)]
     pub struct VerifiedSplit<'info> {
         /// CHECK:
-        #[account(signer)]
+        #[account(signer, mut)]
         pub split_stake: AccountInfo<'info>,
 
         /// CHECK:
         pub authorized: Signer<'info>,
+
+        /// CHECK:
+        #[account(mut)]
+        pub stake: AccountInfo<'info>,
+
+        /// CHECK: Albus ZKP request
+        pub zkp_request: AccountInfo<'info>,
+
+        /// CHECK:
+        pub stake_program: AccountInfo<'info>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+    pub struct VerifiedSplitWithSeed<'info> {
+        /// CHECK:
+        #[account(signer, mut)]
+        pub split_stake: AccountInfo<'info>,
+
+        /// CHECK:
+        pub authorized: Signer<'info>,
+
+        /// CHECK:
+        #[account(signer)]
+        pub base: AccountInfo<'info>,
 
         /// CHECK:
         #[account(mut)]
@@ -371,23 +448,6 @@ pub mod verified_stake {
     }
 
     #[derive(Accounts)]
-    pub struct VerifiedDeactivate<'info> {
-        /// CHECK:
-        #[account(mut)]
-        pub stake: AccountInfo<'info>,
-
-        /// CHECK:
-        pub authorized: Signer<'info>,
-
-        /// CHECK: Albus ZKP request
-        pub zkp_request: AccountInfo<'info>,
-
-        /// CHECK:
-        pub stake_program: AccountInfo<'info>,
-        pub clock: Sysvar<'info, Clock>,
-    }
-
-    #[derive(Accounts)]
     pub struct VerifiedDelegate<'info> {
         /// CHECK:
         #[account(mut)]
@@ -436,5 +496,74 @@ pub mod verified_stake {
 
         /// CHECK:
         pub stake_config: AccountInfo<'info>,
+    }
+
+    #[derive(Accounts)]
+    pub struct VerifiedRedelegateWithSeed<'info> {
+        /// CHECK:
+        #[account(mut)]
+        pub uninitialized_stake: AccountInfo<'info>,
+
+        /// CHECK:
+        #[account(mut)]
+        pub stake: AccountInfo<'info>,
+
+        /// CHECK:
+        #[account(signer)]
+        pub base: AccountInfo<'info>,
+
+        /// CHECK:
+        pub vote: AccountInfo<'info>,
+
+        /// CHECK:
+        pub authorized: Signer<'info>,
+
+        /// CHECK: Albus ZKP request
+        pub zkp_request: AccountInfo<'info>,
+
+        /// CHECK:
+        pub stake_program: AccountInfo<'info>,
+        pub system_program: Program<'info, System>,
+
+        /// CHECK:
+        pub stake_config: AccountInfo<'info>,
+    }
+
+    #[derive(Accounts)]
+    pub struct VerifiedAuthorize<'info> {
+        /// CHECK:
+        #[account(mut)]
+        pub stake: AccountInfo<'info>,
+
+        /// CHECK:
+        pub authorized: Signer<'info>,
+
+        /// CHECK: Albus ZKP request
+        pub zkp_request: AccountInfo<'info>,
+
+        /// CHECK:
+        pub stake_program: AccountInfo<'info>,
+        pub clock: Sysvar<'info, Clock>,
+    }
+
+    #[derive(Accounts)]
+    pub struct VerifiedAuthorizeChecked<'info> {
+        /// CHECK:
+        #[account(mut)]
+        pub stake: AccountInfo<'info>,
+
+        /// CHECK:
+        #[account(signer)]
+        pub new_authorized: AccountInfo<'info>,
+
+        /// CHECK:
+        pub authorized: Signer<'info>,
+
+        /// CHECK: Albus ZKP request
+        pub zkp_request: AccountInfo<'info>,
+
+        /// CHECK:
+        pub stake_program: AccountInfo<'info>,
+        pub clock: Sysvar<'info, Clock>,
     }
 }
