7500e8e6c661cdd0251cbf8fd5e2cb9dec6815a2
tiamo <vk.tiamo@gmail.com>
2023-06-14 12:09:58 +0300

    fix(sdk): add `NFT_AUTHORITY`

--- packages/albus-sdk/src/constants.ts
+++ packages/albus-sdk/src/constants.ts
@@ -2,4 +2,4 @@ export const SERVICE_PROVIDER_SEED_PREFIX = 'service-provider'
 export const ZKP_REQUEST_SEED_PREFIX = 'zkp-request'
 export const NFT_SYMBOL_PREFIX = 'ALBUS'
 
-export const NFT_AUTHORITY = ''
+export const NFT_AUTHORITY = '5tWk9EZcMpdKzxVGr4ZakZDHdWiqVJkLQ1b3v2vraDeH'
