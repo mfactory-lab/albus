967790b7c6335129bf437951b18490cbe5d1d4f0
tiamo <vk.tiamo@gmail.com>
2023-10-03 19:34:29 +0300

    fix(sdk): add `NodeWallet`

--- packages/albus-sdk/src/utils/wallet.ts
+++ packages/albus-sdk/src/utils/wallet.ts
@@ -26,6 +26,42 @@
  * The developer of this program can be contacted at <info@albus.finance>.
  */
 
-export * from './circuit'
-export * from './wallet'
-export * from './web3'
+import type { Wallet } from '@coral-xyz/anchor'
+import type { Keypair, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js'
+
+function isVersionedTransaction(tx: Transaction | VersionedTransaction): tx is VersionedTransaction {
+  return 'version' in tx
+}
+
+export class NodeWallet implements Wallet {
+  constructor(readonly payer: Keypair) {}
+
+  async signTransaction<T extends Transaction | VersionedTransaction>(
+    tx: T,
+  ): Promise<T> {
+    if (isVersionedTransaction(tx)) {
+      tx.sign([this.payer])
+    } else {
+      tx.partialSign(this.payer)
+    }
+
+    return tx
+  }
+
+  async signAllTransactions<T extends Transaction | VersionedTransaction>(
+    txs: T[],
+  ): Promise<T[]> {
+    return txs.map((t) => {
+      if (isVersionedTransaction(t)) {
+        t.sign([this.payer])
+      } else {
+        t.partialSign(this.payer)
+      }
+      return t
+    })
+  }
+
+  get publicKey(): PublicKey {
+    return this.payer.publicKey
+  }
+}
