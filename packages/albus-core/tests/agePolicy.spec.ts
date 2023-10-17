e3a850250d4396a7be8bd4cb953d2e64bf883c74
tiamo <vk.tiamo@gmail.com>
2023-10-17 23:16:28 +0300

    fix(core): agePolicy test fixes

--- packages/albus-core/tests/agePolicy.spec.ts
+++ packages/albus-core/tests/agePolicy.spec.ts
@@ -44,23 +44,14 @@ describe('AgePolicy', async () => {
 
   const timestamp = 1697035401 // 2023-10-11 14:43
   const claims = {
-    // givenName: 'Mikayla',
-    // familyName: 'Halvorson',
-    // gender: 'female',
+    givenName: 'Mikayla',
+    familyName: 'Halvorson',
+    gender: 'female',
     birthDate: '19661002',
-    // birthPlace: 'Westland',
-    // nationality: 'MNE',
-    // country: 'MNE',
-    // countryOfBirth: 'MNE',
-    // address: {
-    //   street1: 'Long road 1',
-    //   street2: 'Long road 1',
-    //   street3: 'Long road 1',
-    //   street4: 'Long road 1',
-    //   state: 'Test',
-    //   city: 'Test',
-    //   house: 5,
-    // },
+    birthPlace: 'Westland',
+    nationality: 'MNE',
+    country: 'MNE',
+    countryOfBirth: 'MNE',
     expirationDate: 0,
   }
 
