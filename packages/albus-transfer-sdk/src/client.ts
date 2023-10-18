821913ef509aa2f6882cedbf503b2eefe56565ab
tiamo <vk.tiamo@gmail.com>
2023-10-19 00:51:04 +0300

    fix(transfer-sdk): add `client`

---
+++ packages/albus-transfer-sdk/src/client.ts
@@ -0,0 +1,74 @@
+import type { AnchorProvider, BN } from '@coral-xyz/anchor'
+import type { ConfirmOptions, PublicKey } from '@solana/web3.js'
+import { Transaction } from '@solana/web3.js'
+import {
+  PROGRAM_ID,
+  createSplTransferInstruction,
+  createTransferInstruction,
+} from './generated'
+
+export class AlbusTransferClient {
+  programId = PROGRAM_ID
+
+  constructor(private readonly provider: AnchorProvider) {}
+
+  get connection() {
+    return this.provider.connection
+  }
+
+  /**
+   * Transfer SOL
+   */
+  async transfer(props: TransferProps, opts?: ConfirmOptions) {
+    const instruction = createTransferInstruction(
+      {
+        proofRequest: props.proofRequest,
+        sender: this.provider.publicKey,
+        receiver: props.receiver,
+      },
+      {
+        amount: props.amount,
+      },
+    )
+
+    const tx = new Transaction().add(instruction)
+    return this.provider.sendAndConfirm(tx, [], opts)
+  }
+
+  /**
+   * Transfer SPL tokens
+   */
+  async transferToken(props: SplTransferProps, opts?: ConfirmOptions) {
+    const instruction = createSplTransferInstruction(
+      {
+        proofRequest: props.proofRequest,
+        sender: this.provider.publicKey,
+        receiver: props.receiver,
+        source: props.source,
+        destination: props.destination,
+        tokenMint: props.tokenMint,
+      },
+      {
+        amount: props.amount,
+      },
+    )
+
+    const tx = new Transaction().add(instruction)
+    return this.provider.sendAndConfirm(tx, [], opts)
+  }
+}
+
+export interface TransferProps {
+  receiver: PublicKey
+  proofRequest: PublicKey
+  amount: BN
+}
+
+export interface SplTransferProps {
+  receiver: PublicKey
+  source: PublicKey
+  destination: PublicKey
+  proofRequest: PublicKey
+  tokenMint: PublicKey
+  amount: BN
+}
