fa53b84ceb68a599844cb2a7316bfc40c00ddf7a
tiamo <vk.tiamo@gmail.com>
2024-03-25 15:41:32 +0200

    test: new credential request test

---
+++ tests/src/credentialRequest.spec.ts
@@ -0,0 +1,155 @@
+/*
+ * This file is part of Albus code.
+ *
+ * Copyright (c) 2023, mFactory GmbH
+ *
+ * Albus is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Albus is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+ *
+ * You can be released from the requirements of the Affero GNU General Public License
+ * by purchasing a commercial license. The purchase of such a license is
+ * mandatory as soon as you develop commercial activities using the
+ * Albus code without disclosing the source code of
+ * your own applications.
+ *
+ * The developer of this program can be contacted at <info@albus.finance>.
+ */
+
+import type { PublicKey } from '@solana/web3.js'
+import { Keypair } from '@solana/web3.js'
+
+import { afterAll, assert, beforeAll, describe, it } from 'vitest'
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token'
+import { AlbusClient } from '../../packages/albus-sdk/src'
+import { assertErrorCode, initMetaplex, initProvider, payer, provider, requestAirdrop } from './utils'
+
+describe('credentialRequest', async () => {
+  const issuer = Keypair.generate()
+  const holder = Keypair.generate()
+
+  const adminClient = new AlbusClient(provider).local()
+  const holderClient = new AlbusClient(initProvider(holder)).local()
+  const issuerClient = new AlbusClient(initProvider(issuer)).local()
+  const mx = initMetaplex(payer)
+
+  const specId = 'AML'
+
+  let mintAddress: PublicKey
+  let issuerAddress: PublicKey
+
+  beforeAll(async () => {
+    await requestAirdrop(payer.publicKey)
+    await requestAirdrop(issuer.publicKey)
+    await requestAirdrop(holder.publicKey)
+
+    const { address } = await adminClient.issuer.create({
+      code: 'aml-issuer',
+      name: 'aml-issuer',
+      keypair: issuer,
+    })
+    issuerAddress = address
+  }, 20000)
+
+  afterAll(async () => {
+    await adminClient.issuer.delete({
+      issuer: issuerAddress,
+    })
+  })
+
+  it('should create a credential spec for the issuer', async () => {
+    const { signature } = await issuerClient.credentialSpec.create({
+      name: specId,
+      issuer: issuerAddress,
+      uri: 'https://pdefinition.json',
+    })
+    assert.ok(!!signature)
+  })
+
+  it('should not allow to create a spec if is not authorized issuer', async () => {
+    try {
+      await holderClient.credentialSpec.create({
+        name: 'test',
+        issuer: issuerAddress,
+      })
+      assert.ok(false)
+    } catch (e: any) {
+      assertErrorCode(e, 'Unauthorized')
+    }
+  })
+
+  it('should allow requesting a credential', async () => {
+    const { mintAddress: address } = await holderClient.credential.create()
+    mintAddress = address
+    const { signature } = await holderClient.credentialRequest.create({
+      mint: mintAddress,
+      issuer: issuerAddress,
+      specId,
+    })
+    assert.ok(!!signature)
+  })
+
+  it('should not allow requesting a credential with unauthorized credentials', async () => {
+    const mint = await createMint(provider.connection, holder, holder.publicKey, null, 0)
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(provider.connection, holder, mint, holder.publicKey)
+    await mintTo(provider.connection, holder, tokenAccount.mint, tokenAccount.address, holder, 1)
+    try {
+      await holderClient.credentialRequest.create({
+        mint,
+        issuer: issuerAddress,
+        specId,
+      })
+      assert.ok(false)
+    } catch (e: any) {
+      assertErrorCode(e, 'Unauthorized')
+    }
+  })
+
+  it('should allow the issuer to update a credential', async () => {
+    const [credentialSpec] = issuerClient.pda.credentialSpec(issuerAddress, specId)
+    const [credentialRequest] = issuerClient.pda.credentialRequest(credentialSpec, holder.publicKey)
+
+    const data = {
+      credentialRequest,
+      uri: 'https://credential.json',
+    }
+
+    const { signature } = await issuerClient.credential.update(data)
+    assert.ok(!!signature)
+
+    const nft = await mx.nfts().findByMint({ mintAddress })
+    assert.equal(data.uri, nft.uri)
+  })
+
+  it('should not allow the holder to update a credential', async () => {
+    const [credentialSpec] = issuerClient.pda.credentialSpec(issuerAddress, specId)
+    const [credentialRequest] = issuerClient.pda.credentialRequest(credentialSpec, holder.publicKey)
+    try {
+      await holderClient.credential.update({
+        credentialRequest,
+        uri: 'https://credential.json',
+      })
+      assert.ok(false)
+    } catch (e: any) {
+      assertErrorCode(e, 'Unauthorized')
+    }
+  })
+
+  it('should allow the issuer to delete a credential spec', async () => {
+    const { signature } = await issuerClient.credentialSpec.delete({
+      name: specId,
+      issuer: issuerAddress,
+    })
+    assert.ok(!!signature)
+  })
+})
