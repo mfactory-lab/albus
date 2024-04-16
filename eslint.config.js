65d21520ed3ddc3670c70635bd43c1d5487149d6
tiamo <vk.tiamo@gmail.com>
2024-04-16 22:17:34 +0300

    fix: fixed pex `require`, upgrade eslint

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  stylistic: true, // enable stylistic formatting rules
+  stylistic: true,
   typescript: true,
   yml: false,
   vue: false,
