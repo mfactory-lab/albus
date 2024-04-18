5219d2823685bf5b7c4f167ff180bdbab8950f8a
tiamo <vk.tiamo@gmail.com>
2024-04-18 14:39:10 +0300

    refactor: improve vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -18,76 +18,6 @@ const inline: string[] = [
   'nist-weierstrauss',
 ]
 
-const isObject = (item: unknown): item is Record<string, unknown> => Boolean(item && typeof item === 'object' && !Array.isArray(item))
-
-function isExternal(source: string) {
-  return !inline.includes(source)
-    && !source.startsWith('.')
-    && !source.startsWith('@/')
-    && !source.startsWith('~/')
-    && !isAbsolute(source)
-}
-
-function mergeDeep<T>(target: T, ...sources: T[]): T {
-  if (!sources.length) {
-    return target
-  }
-  const source = sources.shift()
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) {
-          Object.assign(target, { [key]: {} })
-        }
-        mergeDeep(target[key] as T, source[key] as T)
-      } else {
-        Object.assign(target, { [key]: source[key] })
-      }
-    }
-  }
-
-  return mergeDeep(target, ...sources)
-}
-
-function viteBuild(path: string, options: BuildOptions = {}): BuildOptions {
-  const dir = dirname(fileURLToPath(path))
-  const pkgName = basename(dir)
-
-  return mergeDeep<BuildOptions>(
-    {
-      sourcemap: true,
-      manifest: true,
-      minify: true,
-      reportCompressedSize: true,
-      emptyOutDir: false,
-      lib: {
-        name: `albus_${pkgName}`,
-        entry: resolve(dir, 'src', 'index.ts'),
-        fileName: format => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
-        formats: ['cjs', 'es'],
-      },
-      rollupOptions: {
-        external: isExternal,
-        plugins: [
-          dts({
-            rollupTypes: true,
-            // insertTypesEntry: true,
-          }),
-          nodePolyfills({
-            protocolImports: true,
-          }),
-          // inject({ Buffer: ['buffer', 'Buffer'] }) as PluginOption,
-        ],
-        output: {
-          dir: resolve(dir, 'dist'),
-        },
-      },
-    },
-    options,
-  )
-}
-
 /**
  * Config for plugins
  *
@@ -115,6 +45,50 @@ export function pluginViteConfig(packageDirName: string, options: UserConfig = {
   } as UserConfig)
 }
 
+function viteBuild(path: string, options: BuildOptions = {}): BuildOptions {
+  const dir = dirname(fileURLToPath(path))
+  const pkgName = basename(dir)
+
+  return mergeDeep<BuildOptions>(
+    {
+      sourcemap: true,
+      manifest: true,
+      minify: true,
+      reportCompressedSize: true,
+      emptyOutDir: false,
+      lib: {
+        name: `albus_${pkgName}`,
+        entry: resolve(dir, 'src', 'index.ts'),
+        fileName: format => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
+        formats: ['cjs', 'es'],
+      },
+      rollupOptions: {
+        external: (source: string) => {
+          return !inline.includes(source)
+            && !source.startsWith('.')
+            && !source.startsWith('@/')
+            && !source.startsWith('~/')
+            && !isAbsolute(source)
+        },
+        plugins: [
+          dts({
+            rollupTypes: true,
+            // insertTypesEntry: true,
+          }),
+          nodePolyfills({
+            protocolImports: true,
+          }),
+          // inject({ Buffer: ['buffer', 'Buffer'] }) as PluginOption,
+        ],
+        output: {
+          dir: resolve(dir, 'dist'),
+        },
+      },
+    },
+    options,
+  )
+}
+
 // export default defineConfig({
 //   test: {
 //     globals: true,
@@ -124,3 +98,27 @@ export function pluginViteConfig(packageDirName: string, options: UserConfig = {
 //     testTimeout: 20000,
 //   },
 // } as any)
+
+const isObject = (item: unknown): item is Record<string, unknown> => Boolean(item && typeof item === 'object' && !Array.isArray(item))
+
+function mergeDeep<T>(target: T, ...sources: T[]): T {
+  if (!sources.length) {
+    return target
+  }
+  const source = sources.shift()
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} })
+        }
+        mergeDeep(target[key] as T, source[key] as T)
+      } else {
+        Object.assign(target, { [key]: source[key] })
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources)
+}
